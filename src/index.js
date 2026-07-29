79f894a03435fe50da543cc6760417ea955e65e9
Pranav <pranavkavade9@gmail.com>
2026-07-29 15:30:56 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse utilities

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
-    path:'./env'
+    path: './env'
 })
 
+// Because we use aync-await it will provide/ return promises.
 connectDB()
-
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port:${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!", err);
+    })
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// Method 2: Wrapper Utility
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler).catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+
+
+
+
+// Higher Order functions
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async() => {}
+
+// Method 1: Wrapper
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: err.message
+//         }) // Help for fortend debugging.
+//     }
+// }
